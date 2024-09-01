f9d284510f912805c9c8f0f9db0c53adce4ab048
Chiya Pandey <145787386+chiyapandey@users.noreply.github.com>
2024-09-01 17:14:30 +0530

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,9 @@
-import React, { useContext, useEffect, useState } from 'react'
-import './Home.css'
-import { CoinContext } from '../../context/Coin.Context'
-import {Link} from 'react-router-dom'
+import React, { useContext, useEffect, useState } from 'react';
+import './Home.css';
+import { CoinContext } from '../../context/Coin.Context';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
-
   const { allCoin, currency } = useContext(CoinContext);
   const [displayCoin, setDisplayCoin] = useState([]);
   const [input, setInput] = useState('');
@@ -14,34 +13,43 @@ const Home = () => {
     if (event.target.value === "") {
       setDisplayCoin(allCoin);
     }
-  }
+  };
 
   const searchHandler = async (event) => {
     event.preventDefault();
-    const coins = await allCoin.filter((item) => {
-      return item.name.toLowerCase().includes(input.toLowerCase())
-    })
+    const coins = await allCoin.filter((item) =>
+      item.name.toLowerCase().includes(input.toLowerCase())
+    );
     setDisplayCoin(coins);
-  }
+  };
 
   useEffect(() => {
     setDisplayCoin(allCoin);
-  }, [allCoin])
+  }, [allCoin]);
 
   return (
-    <div className='home'>
+    <div className="home">
       <div className="hero">
         <h1>Explore The Future Of Cryptocurrency</h1>
-        <p>Enter the leading place for all things cryptocurrency. Sign up today to gain exclusive access to industry insights and the most recent crypto developments.</p>
+        <p>
+          Enter the leading place for all things cryptocurrency. Sign up today
+          to gain exclusive access to industry insights and the most recent
+          crypto developments.
+        </p>
         <form onSubmit={searchHandler}>
-
-          <input onChange={inputHandler} list='coinlist' value={input} type="text"
-            placeholder='Search crypto..' required />
-
-          <dataList id='coinlist'>
-            {allCoin.map((item, index) => (<option key={index} value={item.name} />))}
-          </dataList>
-
+          <input
+            onChange={inputHandler}
+            list="coinlist"
+            value={input}
+            type="text"
+            placeholder="Search crypto.."
+            required
+          />
+          <datalist id="coinlist">
+            {allCoin.map((item, index) => (
+              <option key={index} value={item.name} />
+            ))}
+          </datalist>
           <button type="submit">Search</button>
         </form>
       </div>
@@ -50,28 +58,35 @@ const Home = () => {
           <p>#</p>
           <p>Coins</p>
           <p>Price</p>
-          <p style={{ textAlign: "center" }}>24H Change</p>
-          <p className='market-cap'>Market Cap</p>
+          <p style={{ textAlign: 'center' }}>24H Change</p>
+          <p className="market-cap">Market Cap</p>
         </div>
-        {
-          displayCoin.slice(0, 10).map((item, index) =>(
-            <Link to={`/coin/${item.id}`} className="table-layout" key={index}>
-              <p>{item.market_cap_rank}</p>
-              <div>
-                <img src={item.image} alt="" />
-                <p>{item.name + " - " + item.symbol}</p>
-              </div>
-              <p>{currency.symbol} {item.current_price.toLocaleString()}</p>
-              <p className={item.price_change_percentage_24h > 0 ? "green":"red"}>
-                {Math.floor(item.price_change_percentage_24h * 100)/100}</p>
-              <p className='market-cap'> {currency.symbol} {item.market_cap.toLocaleString()}</p>
-            </Link>
-          ))
-        }
-
+        {displayCoin.slice(0, 10).map((item, index) => (
+          <Link to={`/coin/${item.id}`} className="table-layout" key={index}>
+            <p>{item.market_cap_rank}</p>
+            <div>
+              <img src={item.image} alt="" />
+              <p>{item.name + ' - ' + item.symbol}</p>
+            </div>
+            <p>
+              {currency.symbol} {item.current_price.toLocaleString()}
+            </p>
+            <p
+              className={
+                item.price_change_percentage_24h > 0 ? 'green' : 'red'
+              }
+            >
+              {Math.floor(item.price_change_percentage_24h * 100) / 100}
+            </p>
+            <p className="market-cap">
+              {currency.symbol} {item.market_cap.toLocaleString()}
+            </p>
+          </Link>
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
+
